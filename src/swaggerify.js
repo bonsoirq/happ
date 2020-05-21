d4813403eb1f5ec925d93d2d2672f61b5aa64354
Jarosław Bąk <jaroslaw.bak24@gmail.com>
2020-05-21 16:24:16 +0200

    switch yamljs to yaml

--- src/swaggerify.js
+++ src/swaggerify.js
@@ -1,9 +1,16 @@
 const swaggerUi = require('swagger-ui-express')
-const yaml = require('yamljs')
-const swaggerDoc = yaml.load('./swagger.yaml')
+const path = require('path')
+const fs = require('fs')
+const yaml = require('yaml')
 
 async function swaggerify (app) {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc()))
+}
+
+function swaggerDoc () {
+  const filepath = path.join(__dirname, '../swagger.yaml')
+  const file = fs.readFileSync(filepath, { encoding: 'utf8' })
+  return yaml.parseDocument(file)
 }
 
 module.exports = {
