c670ec07c4dfabf7d5ef5598cd0588e0e03ead6f
Maksymilian Galas <maksg@onet.eu>
2020-06-12 00:12:20 +0200

    add create happening button

--- web/src/views/list-happenings.js
+++ web/src/views/list-happenings.js
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 import Table from 'components/table/table';
 import Button from 'components/button';
 import HappeningDetailsView from './happening-details';
+import ModalCard from 'components/modal/modal-card';
+import ModalCardHead from 'components/modal/modal-card-head';
+import ModalCardContent from 'components/modal/modal-card-content';
+import ModalCardFoot from 'components/modal/modal-card-foot';
 
 export default function ListHappeningsView(props) {
+  const [showCreateHappeningModal, setShowCreateHappeningModal] = useState(false);
   const [selectedDetailsHappening, selectDetailsHappening] = useState(null);
 
   return (<>
     <Table>
-      <HappeningTableHeader />
+      <HappeningTableHeader onClick={() => setShowCreateHappeningModal(true)} />
       <tbody>
         {/* TODO: custom row for empty array */}
         {props.happenings.map(x => (
@@ -33,14 +38,39 @@ export default function ListHappeningsView(props) {
         onClose={() => selectDetailsHappening(null)}
       />
     }
+    {
+      showCreateHappeningModal &&
+      <ModalCard>
+        <ModalCardHead
+          title="Create new happening"
+          onClose={() => setShowCreateHappeningModal(false)}
+        />
+        <ModalCardContent>
+          TODO: Form
+        </ModalCardContent>
+        <ModalCardFoot>
+          <Button
+            onClick={() => setShowCreateHappeningModal(false)}
+          >
+            Cancel
+          </Button>
+        </ModalCardFoot>
+      </ModalCard>
+    }
   </>
   )
 }
 
-function HappeningTableHeader() {
+function HappeningTableHeader({onClick}) {
   return <thead>
     <th>Name</th>
     <th>Description</th>
-    <th></th>
+    <th>
+      <Button
+        onClick={onClick}
+      >
+        Create
+      </Button>
+    </th>
   </thead>
 }
