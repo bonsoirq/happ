ce0b2d3bc763fe746e36d096fa44c4ac751125cc
Maksymilian Galas <maksg@onet.eu>
2020-06-11 23:04:16 +0200

    feat: add happening details view

--- web/src/pages/account.js
+++ web/src/pages/account.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import DashboardLayout from 'layouts/dashboard-layout';
-import AccountInfoView from 'views/account-info-view';
+import AccountInfoView from 'views/account-info';
 
 export default function Account () {
   return (
