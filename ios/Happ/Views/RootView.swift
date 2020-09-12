d7524e9c372fcd4d2ae6a56247253cad775ec3ac
Maksymilian Galas <maksg@onet.eu>
2020-09-12 20:04:15 +0200

    create ios project

---
+++ ios/Happ/Views/RootView.swift
@@ -0,0 +1,21 @@
+//
+//  RootView.swift
+//  Happ
+//
+//  Created by Maksymilian Galas on 12/09/2020.
+//  Copyright © 2020 Infinity Pi Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
